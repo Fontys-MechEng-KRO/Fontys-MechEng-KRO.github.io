8f53d993d4279345fb728249c37994bc5b679b55
Fontys-MechEng-KRO <83716798+Fontys-MechEng-KRO@users.noreply.github.com>
2021-05-09 22:06:24 +0200

    Update testbutton.js

--- testbutton.js
+++ testbutton.js
@@ -306,7 +306,7 @@ buttonclearrow.addEventListener('click', function(){
     alert('The table is empty. There are no rows to delete.');
   }
   
-  if else{
+  else{
    
       
     matrix[counter-1]= [x1,x2,x3,x4,x5,x6];
